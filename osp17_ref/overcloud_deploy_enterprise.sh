74b371e333ddf85f17d4c701cf5dab8e56d43acb
vcandappa <vcandapp@redhat.com>
2022-12-07 17:04:42 +0530

    Adding scripts for enterprise

--- osp17_ref/overcloud_deploy_enterprise.sh
+++ osp17_ref/overcloud_deploy_enterprise.sh
@@ -16,11 +16,10 @@ USER_THT="$HOME/osp17_ref"
 #fi
 
 echo "Creating roles..."
-openstack overcloud roles generate -o $HOME/roles_data.yaml ControllerSriov ComputeOvsDpdkSriov
+openstack overcloud roles generate -o $HOME/roles_data.yaml Controller Compute
 
 openstack overcloud deploy $PARAMS \
     --templates /usr/share/openstack-tripleo-heat-templates \
-    --timeout 120 \
     --stack overcloud \
     --network-config \
     -r /home/stack/roles_data.yaml \
@@ -28,14 +27,11 @@ openstack overcloud deploy $PARAMS \
     -e /home/stack/templates/overcloud-baremetal-deployed.yaml \
     -e /home/stack/templates/overcloud-networks-deployed.yaml \
     -e /home/stack/templates/overcloud-vip-deployed.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-dpdk.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-sriov.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/debug.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/config-debug.yaml \
     -e /home/stack/osp17_ref/environment.yaml \
     -e /home/stack/osp17_ref/network-environment.yaml \
-    -e /home/stack/osp17_ref/network-environment-regular.yaml \
     -e /home/stack/osp17_ref/ml2-ovs-nfv.yaml \
     -e /home/stack/containers-prepare-parameter.yaml \
     --ntp-server clock.redhat.com,time1.google.com,time2.google.com,time3.google.com,time4.google.com \
